4aaf6b62f439966e9fa522979d79657548466958
Radek Beran <beran.radek@seznam.cz>
2015-07-11 17:56:42 +0200

    Libraries for servlets 3 with Jetty

--- src/main/java/net/formio/demo/HttpServer.java
+++ src/main/java/net/formio/demo/HttpServer.java
@@ -4,9 +4,17 @@ import java.io.File;
 import java.net.URL;
 
 import org.apache.jasper.servlet.JspServlet;
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.FragmentConfiguration;
+import org.eclipse.jetty.webapp.MetaInfConfiguration;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+import org.eclipse.jetty.webapp.WebXmlConfiguration;
 
 /**
  * Main application (embedded Jetty container).
@@ -20,10 +28,7 @@ public class HttpServer {
 		// the initialization phase of your application
 		// SLF4JBridgeHandler.install();
 		
-		String portStr = System.getenv("PORT");
-		if (portStr == null || portStr.isEmpty()) portStr = "8080";
-		// if (portStr == null || portStr.isEmpty()) throw new IllegalStateException("Port not specified, please set environment variable PORT");
-		
+		// Required JSP support for Jetty
 		ServletHolder jspServletHolder = new ServletHolder("jspServlet", new JspServlet());
 		// these two lines are not strictly required - they will keep classes generated from JSP in "${javax.servlet.context.tempdir}/views/generated"
 		jspServletHolder.setInitParameter("keepgenerated", "true");
@@ -35,17 +40,38 @@ public class HttpServer {
         context.setClassLoader(Thread.currentThread().getContextClassLoader());
         context.setContextPath("/");
         context.addServlet(jspServletHolder, "*.jsp");
+        
         // taken from web.xml:
         // context.setDisplayName("Formio demo");
         // context.setWelcomeFiles(new String[] { "simple.html" });
-        // context.addServlet(new ServletHolder(new SimpleController()), "/");
-        // context.addServlet(new ServletHolder(new AdvancedController()), "/advanced.html");
-        context.setResourceBase(getBaseUrl());
-        Server server = new Server(Integer.valueOf(portStr).intValue());
+        
+        String baseUrl = getBaseUrl();
+        context.setResourceBase(baseUrl);
+        context.setDescriptor(baseUrl + "/WEB-INF/web.xml");
+        // context.setDescriptor(WEB_APP_ROOT);
+        context.setConfigurations(new Configuration[] {
+			new AnnotationConfiguration(), new WebXmlConfiguration(),
+			new WebInfConfiguration(),
+			new PlusConfiguration(), new MetaInfConfiguration(),
+			// Enable parsing of jndi-related parts of web.xml and jetty-env.xml:
+			new FragmentConfiguration(), new EnvConfiguration() });
+        context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",".*/classes/.*|.*/controller/.*");
+        context.setParentLoaderPriority(true);
+        context.setConfigurationDiscovered(true);
+        
+        Server server = new Server(getServerPort());
         server.setHandler(context);
+     
         server.start();
+        // server.dump(System.err);
         server.join();
     }
+
+	private static int getServerPort() {
+		String portStr = System.getenv("PORT");
+		if (portStr == null || portStr.isEmpty()) portStr = "8080";
+		return Integer.valueOf(portStr).intValue();
+	}
 	
 	private static String getBaseUrl() {
 		URL webInfUrl = HttpServer.class.getClassLoader().getResource(WEB_APP_ROOT);
--- src/main/java/net/formio/demo/controller/SimpleController.java
+++ src/main/java/net/formio/demo/controller/SimpleController.java
@@ -6,6 +6,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
